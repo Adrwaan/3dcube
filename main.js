dfc7ecbc213d45840d5eb1f1ff380cab28280827
Adrwaan <adrean.danilo113@gmail.com>
2023-12-17 20:39:44 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import * as THREE from "./node_modules/three/build/three.js";
+import { OrbitControls } from "./node_modules/three/examples/jsm/controls/OrbitControls";
 
 // Set global variables
 var camera, scene, renderer, mesh, controls;
